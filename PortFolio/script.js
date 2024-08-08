19f1a84821385877b05cdbd6248c39fd4c20998f
viki-vyas <weneda9364@bsomek.com>
2024-08-08 12:13:16 +0530

    Portfolio updated some feature

--- PortFolio/script.js
+++ PortFolio/script.js
@@ -5,7 +5,7 @@ const dropdownMenu = document.querySelector('.dropdown_bar')
 toggleBtn.addEventListener('click', ()=>{
     // dropdownBar.classList.toggle('open').
     dropdownMenu.classList.toggle("open");
-    console.log("iam alive")
+    // console.log("iam alive")
     const isOpen = dropdownMenu.classList.contains("open");
     toggleBtnIcon.classList = isOpen? "fa-solid fa-xmark":"fa-solid fa-bars";
 })
